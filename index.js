5f9f03449150ce676aba3b782b0e779b999db00d
Solane-tiyya <solanesani1@gmail.com>
2024-08-29 13:43:52 +0200

    Add page

---
+++ index.js
@@ -0,0 +1,46 @@
+//console.log("Hello Worlid");
+//const currentYear =2021;
+//const birthYear = 1986;
+//const age = currentYear - birthYear;
+//const is overforty =age > 40;
+
+//console.log(age);
+//console.log;
+//const favoriteDrink = "Coffe";
+
+//const myArray = ["Cat", "Dog"];
+//console.log ("My Arry, myArry");
+//cont a =2;
+//const b = "2";
+//console.log("First coomp equality", a=b);
+//console.log("Seconde comp equality with type b");
+
+
+
+//const myObject ={
+   // age: 35,
+   // name:{
+       // first: "Solane",
+        //last: "Hebo",
+   // }
+
+//};
+//console.log("User age", user.age);
+//console.log("user first name", user.name.first);
+
+brand = "Audi"
+modelYear = 2000
+model = "A3"
+currentMileage = 8000
+bigServiceMileage = 6000
+console.log("brand", brand);
+console.log("modelYear", modelYear);
+
+//console.message = `Du äger en som har körts i {currentMileage} km");
+console.log("currentMileage", currentMileage);
+console.log("bigServiceMileage", bigServiceMileage);
+
+
+
+
+
